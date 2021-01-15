30ec4ea9af176128aa8ba9e7eb9e084386ea7dc4
mehmetserifpasa <=>
2021-01-15 12:01:54 +0300

    .

---
+++ 5-hafta Dark-Tema/main.js
@@ -0,0 +1,10 @@
+var body = document.querySelector('body');
+var dark = document.getElementById('dark');
+
+
+dark.addEventListener('click', function() {
+    body.classList.toggle("DarkMode");
+
+})
+
+
